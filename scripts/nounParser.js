680743f5f3808e02e147684dca86e737268762bb
Samuel Warner <samuelrwarner1@gmail.com>
2016-07-18 16:27:20 -0400

    put utility functions back into nounParser for scope and refactored other utility functions

--- scripts/nounParser.js
+++ scripts/nounParser.js
@@ -1,35 +1,80 @@
 function Noun(meaning, dictEntry, types, chapter, section){
+	var self = this;
 	this.firstDict = dictEntry.trim().match(/^[a-zA-Z]+(?=,)/).join();
 	//Order Matters.  Gender must be in front of Declension to remove any of the info.
 	//For words that don't have a declared gender those are dealt with under the declension section.
-	this.gender = getGender(dictEntry);
-	this.declension = undefined;
-		if(dictEntry.match(/i$/)){
-			this.declension = '2nd';
-			if(this.gender === undefined){
-				if(dictEntry.match(/um(?=,)/)){this.gender = 'N'; }
-				else{ this.gender = 'M';}
-			}
-		}
-		if(dictEntry.match(/ae$/)){this.declension = '1st';}
-				if(this.gender === undefined){this.gender = 'F';}
-
-		if(dictEntry.match(/is$/)){this.declension = '3rd';}
-		if(dictEntry.match(/ei$/)){this.declension = '5th';}
-		if(dictEntry.match(/us$/)){this.declension = '4th';}
-	this.stem = undefined;
-		if(this.declension == '2nd'){
-			if(dictEntry.match(/[a-z](?=,)/).join() == 'r') {
-				this.stem = dictEntry.match(/[a-zA-Z]+(?=,)/);
-			}
-			else if(this.gender == 'M'){
-				this.stem = dictEntry.match(/[a-zA-Z]+(?=us,)/i).join();
-			}
-			else{ this.stem = dictEntry.match(/[a-zA-Z]+(?=um,)/i).join();}
-		}
-		if(this.declension == '1st'){this.stem = dictEntry.match(/[a-zA-Z]+(?=a,)/i).join();}
+	this.gender = getGender();
+	this.declension = getDeclension();
+	this.stem = getStem();
 	this.meaning = meaning;
 	this.types = types;
 	this.chapter = chapter;
 	this.section = section;
+
+	function getGender(){
+	  //this.gender = undefined;
+	    //clean up the parentheses if there are any
+	    dictEntry = dictEntry.replace(/\(/, '');
+	    dictEntry = dictEntry.replace(/\)/, '');
+	    //check to see if both masculine and feminine are given for common gender
+	      if(dictEntry.match(/m\/f$/i)){
+	        dictEntry = dictEntry.replace(/ m\/f$/i, '' );
+	        return 'C';
+	      }
+	      if(dictEntry.match(/c$/i)){
+	        dictEntry = dictEntry.replace(/ c$/i,'' );
+	        return 'C';
+	      }
+	      if(dictEntry.match(/m$/i)){
+	        dictEntry = dictEntry.replace(/ m$/i,'' );
+	        return 'M';
+	      }
+	      if(dictEntry.match(/f$/i)){
+	        dictEntry = dictEntry.replace(/ f$/i,'' );
+	        return 'F';
+	      }
+	      if(dictEntry.match(/n$/i)){
+	        dictEntry = dictEntry.replace(/ n$/i,'' );
+	        return 'N';
+	      }
+	}
+	function getDeclension(){
+		//clean the data from extra spaces at the end
+		dictEntry = dictEntry.trim();
+		//check endings to get undefined genders and declensions
+		if(dictEntry.match(/i$/)){
+			//if the gender was not stated in the dictionary entry pull it from the default types of declensions
+			if(self.gender === undefined){
+				if(dictEntry.match(/um(?=,)/)){self.gender = 'N'; }
+				else{ self.gender = 'M';}
+			}
+			return '2nd';
+		}
+		if(dictEntry.match(/ae$/)){
+			if(self.gender === undefined){
+				self.gender = 'F';}
+			return '1st';
+		}
+		if(dictEntry.match(/is$/)){return '3rd';}
+		if(dictEntry.match(/ei$/)){return '5th';}
+		if(dictEntry.match(/us$/)){return '4th';}
+	}
+	function getStem(){
+		if(self.declension === '2nd'){
+			//check for words like puer, vir, ager, etc... that would form a stem as
+			//its first dictionary entry
+			if(dictEntry.match(/[a-z](?=,)/).join() === 'r') {
+				return dictEntry.match(/[a-zA-Z]+(?=,)/);
+			}
+			else if(self.gender === 'M'){
+				return dictEntry.match(/[a-zA-Z]+(?=us,)/i).join();
+			}
+			else{
+				return dictEntry.match(/[a-zA-Z]+(?=um,)/i).join();
+			}
+		}
+		if(self.declension === '1st'){
+			return dictEntry.match(/[a-zA-Z]+(?=a,)/i).join();
+		}
+	}
 }
--- scripts/nounShortList.js
+++ scripts/nounShortList.js
@@ -3,6 +3,7 @@ var dictionary = {
     puella	:  new Noun("	girl	","	puella, -ae	",["	person	"], "	II	",	1	),
     servus	:  new Noun("	slave	","	servus, -i	",["	person	"], "	II	",	2	),
     oppidum	:  new Noun("	city	","	oppidum, -i	",["	place	"], "	I	",	1	),
+    rex : new Noun("king", " rex, regis (M)", ["person"], "X", 3)
   }
 };
 console.log(dictionary);
