d17f9b2f03844de6969c0809994695bd50f23c6c
Samuel Warner <samuelrwarner1@gmail.com>
2016-07-25 17:29:21 -0400

    added subject endings

--- scripts/modules/pages/sentenceGen/sentence_controller.js
+++ scripts/modules/pages/sentenceGen/sentence_controller.js
@@ -7,19 +7,30 @@ app.config(['$routeProvider', function($routeProvider){
 }]);
 
 app.controller('sentenceController', ['$scope', function($scope){
-  var randomNumber = random(['sg','pl']);
+  var randomNumber = random(['sg']);
   $scope.nouns = dictionary.nouns;
-  $scope.subject = pickNoun('person');
+  $scope.subject = subject();
   $scope.place = pickNoun('place');
   $scope.placeEnding = grammar.ablative[randomNumber][$scope.place.declension + $scope.place.gender];
   $scope.directObject = pickNoun('person');
   // $scope.verbs = dictionary.verbs;
 
+
+  function subject(){
+    var subject = pickNoun('person');
+    if(randomNumber === 'sg'){
+      subject.stem = subject.firstDict;
+      subject.ending = '';
+    }
+    else {
+      subject.ending = grammar.nominative[randomNumber][subject.declension + subject.gender];
+    }
+    return subject;
+  }
   //random takes an array and gives back a random value of that array
   function random(arr){
     return arr[Math.floor(Math.random() * arr.length)];
   }
-
   //pickNoun takes a string to check the type of a noun
   function pickNoun(type){
     return random(dictionary.nouns.filter( function(x){
