b1c4431c879eae6d03840e19f775ab2c3cdf1400
Samuel Warner <samuelrwarner1@gmail.com>
2016-08-02 14:44:56 -0400

    added verbGrammar.js

---
+++ scripts/verbGrammar.js
@@ -0,0 +1,57 @@
+grammar.verbs = {
+  connectingVowels : {
+    present: {
+      first: 'a',
+      second: 'e',
+      third: 'i',
+      thirdIo: 'i',
+      fourth: 'i',
+    },
+    imperfect:{
+      first: 'a',
+      second: 'e',
+      third: 'e',
+      thirdIo: 'ie',
+      fourth: 'ie',
+    },
+  },
+  personalEndings : {
+    present : {
+      active: {
+        firstPersonsg: 'o',
+        secondPersonsg: 's',
+        thirdPersonsg: 't',
+        firstPersonpl: 'mus',
+        secondPersonpl: 'tis',
+        thirdPersonpl: 'nt'
+      },
+      passive: {
+        firstPersonsg: 'or',
+        secondPersonsg: 'ris',
+        thirdPersonsg: 'tur',
+        firstPersonpl: 'mur',
+        secondPersonpl: 'mini',
+        thirdPersonpl: 'ntur'
+      }
+    },
+    perfect : {
+      active: {
+        firstPersonsg: 'i',
+        secondPersonsg: 'isti',
+        thirdPersonsg: 'it',
+        firstPersonpl: 'imus',
+        secondPersonpl: 'istis',
+        thirdPersonpl: 'erunt'
+      },
+      passive: {
+        firstPersonsg: ' sum',
+        secondPersonsg: ' es',
+        thirdPersonsg: ' est',
+        firstPersonpl: ' sumus',
+        secondPersonpl: ' estis',
+        thirdPersonpl: ' erunt'
+      }
+    }
+  }
+
+};
