a893efe1b446e9c3393980578d94f3d132f21e22
Samuel Warner <samuelrwarner1@gmail.com>
2016-08-02 12:45:57 -0400

    added verb parser

---
+++ scripts/parsers/irregularVerb.js
@@ -0,0 +1,4 @@
+// dictionary.irregularVerbs = [
+//   new Verb("	to fetch; carry towards	","	affero, afferre, attuli, allatus	","	t	","	XIV	",	1	),
+// new Verb("	(a) to say	","	aio, ais, ait, aiunt	","	t	","	XXI	",	2	),
+// ];
